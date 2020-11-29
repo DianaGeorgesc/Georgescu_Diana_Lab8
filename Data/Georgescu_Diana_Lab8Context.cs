6221b02cbde55ded47146f3cbb85bf18220e4393
diana.georgescu1999@gmail.com <diana.georgescu1999@gmail.com>
2020-11-29 16:14:46 +0200

    Add project files.

---
+++ Data/Georgescu_Diana_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Georgescu_Diana_Lab8.Models;
+
+namespace Georgescu_Diana_Lab8.Data
+{
+    public class Georgescu_Diana_Lab8Context : DbContext
+    {
+        public Georgescu_Diana_Lab8Context (DbContextOptions<Georgescu_Diana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Georgescu_Diana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Georgescu_Diana_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
